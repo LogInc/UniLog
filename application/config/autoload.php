71bd88db099ee1bb97cda142be8cedabd89ace72
Abdullah Baig <abdullahbaig456@gmail.com>
2015-04-03 22:12:32 +0500

    Fixed the sign-in/up page markup. Added custom helper file and added methods for quick access to css, images and js.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -88,7 +88,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'html');
+$autoload['helper'] = array('url', 'html', 'custom');
 
 
 /*
