a3d69edc1c1f2e8014a64a87ba0187267ca53062
ahmarsultan <ahmar_sultan@live.com>
2015-04-17 11:19:51 +0500

    Sign_up wall updated and User_wall  in progress

--- application/views/forms/sign_up.php
+++ application/views/forms/sign_up.php
@@ -7,52 +7,96 @@
 ?>
 
 <div class="col-md-5">
-	<?php echo form_open('sign-up', 'class="form-horizontal" role="form"') ?>
     <h2><b>Sign Up</b></h2>
-    <h4 class="text-danger"><?php echo validation_errors(); ?></h4>
+    
     <p> or <a href="<?php echo site_url('sign-in'); ?>">Sign In</a></p>
-
-    <div class="form-group form-group-lg">
-        <label class="control-label col-lg-1 col-md-4 col-sm-0 sr-only" for="firstname">First Name:</label>
-        <div class="col-md-10 col-sm-9">
-            <input class="form-control" type="text" name="firstname" value="<?php echo set_value('firstname'); ?>" placeholder="Enter your first name">
+    
+    <div id="form_type" class="form-inline">    
+        <p> Select account type </p>
+        <div class ="col-lg-10 col-md-9">
+            <div class="radio">
+                
+                <label><input  id ="U-S"type="radio" name="type" value="1"> University Student</label>
+            </div>
         </div>
+        <div class ="col-lg-10 col-md-9">
+            
+            <div class="radio ">
+                <label><input  type="radio" name="type" value=2> Open Course Student</label>
+            </div>
+        </div>
+        
+        
+        <div class="form-group form-group-lg">
+            <br>
+            <div class="col-lg-offset-10 col-md-10 col-md-offset-8 col-sm-9 col-sm-offset-5 pull-right">
+                <button class="btn btn-primary moveOn" type="Next" >Ok, next!</button>
+            </div>
+        </div>
+        
+    
+        
     </div>
+</div>
 
+<div class="col-md-5">
+    
+	<?php echo form_open('sign-up', 'class="form-horizontal" role="form"') ?>
+    <h4 class="text-danger"><?php echo validation_errors(); ?></h4>    
+    
+    <form role="form">      
+        <div class = "Student-form" style = "display:none">
+            <div class="form-group form-group-lg ">
+                <label class="control-label col-lg-1 col-md-4 col-sm-0 sr-only" for="firstname">First Name:</label>
+                <div class="col-md-10 col-sm-9">
+                    <input class="form-control" type="text" name="firstname" value="<?php echo set_value('firstname'); ?>" placeholder="Enter your first name">
+                </div>
+            </div>
+    </form> 
+    
     <div class="form-group form-group-lg">
         <label class="control-label col-lg-1 col-md-4 col-sm-0 sr-only" for="lastname">Last Name:</label>
         <div class="col-md-10 col-sm-9">
             <input class="form-control" type="text" name="lastname" value="<?php echo set_value('lastname'); ?>" placeholder="Enter your last name">
         </div>
     </div>
-
-    <div class="form-group form-group-lg">
+    
+    
+    <div class="form-group form-group-lg roll_num" style = "display:none">
         <label class="control-label col-lg-1 col-md-4 col-sm-0 sr-only" for="rollno">Roll No:</label>
         <div class="col-md-10 col-sm-9">
             <input class="form-control" type="text" name="rollno" value="<?php echo set_value('rollno'); ?>" placeholder="Enter your roll no.">
         </div>
     </div>
-
+    
+    <div class="form-group form-group-lg">
+        <label class="control-label col-lg-1 col-md-4 col-sm-0 sr-only" for="email">PIN:</label>
+        <div class="col-md-10 col-sm-9">
+            <input class="form-control" type="text" name="email" value="<?php echo set_value('pin'); ?>" placeholder="Enter your PIN">
+        </div>
+    </div>
+    
+    
     <div class="form-group form-group-lg">
         <label class="control-label col-lg-1 col-md-4 col-sm-0 sr-only" for="email">Email:</label>
         <div class="col-md-10 col-sm-9">
             <input class="form-control" type="text" name="email" value="<?php echo set_value('email'); ?>" placeholder="Enter your email address">
         </div>
     </div>
-
+    
     <div class="form-group form-group-lg">
         <label class="control-label col-lg-1 col-md-4 col-sm-0 sr-only" for="password">Password:</label>
         <div class="col-md-10 col-sm-9">
             <input class="form-control" type="password" name="password" placeholder="Enter your password">
         </div>
     </div>
-
+    
     <div class="form-group form-group-lg">
         <div class="col-lg-offset-1 col-md-10 col-md-offset-0 col-sm-9 col-sm-offset-0">
             <button class="btn btn-primary btn-lg btn-block" type="submit" name="submit_signup">Sign Up</button>
         </div>
     </div>
-
+    
     <div class="form-group form-group-lg">
         <div class="col-md-9 col-md-offset-1 col-sm-8 col-sm-offset-0">
             <label class="form-control-static">By signing up, you agree to UniLog's
@@ -61,3 +105,29 @@
     </div>
 	<?php echo '</form>'; ?>
 </div>
+</div>
+
+<script>
+    $(document).ready(function(){
+        $(".moveOn").click(function(){
+            
+            
+            var value =  $('input[name=type]:checked', '#form_type').val()
+            
+            if(value === "1")
+            {
+                $("#form_type").hide();
+                $(".Student-form").fadeIn();
+                $(".roll_num").fadeIn();
+            }
+            if(value == "2")
+            {
+                $("#form_type").hide();
+                $(".Student-form").fadeIn();
+                
+            }
+            
+        });
+    });
+</script>
+
---
+++ application/views/user_wall.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+ * UniLog project.
+ * UniLog is an on-line educational courseware for the University of Engineering and Technology, Lahore.
+ * Copyright 2015 log inc.
+ */
+require_once 'templates/page_head.php';
+require_once 'templates/brand.php';
+
+?>
+
+<div class="col-md-6">
+    <a href='#'> Ahmar Sultan </a>
+</div>
+
+    
+    
+    
+    
+    
+<?php
+require_once 'templates/page_foot.php';
+?>
