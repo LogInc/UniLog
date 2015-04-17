332009f72633bb737ac7fa820c71249faa2e4944
Abdullah Baig <abdullahbaig456@gmail.com>
2015-04-17 18:45:44 +0500

    Added setup steps in the readme file. Changed the name of student_pin table to student_auth. Delete the useless favicon.gif file.

--- db/create_db.php
+++ db/create_db.php
@@ -15,7 +15,7 @@
  *	temp_user
  *	temp_student
  *  student
- *	student_pin
+ *	student_auth
  *	course
  *	ci_session
  */
@@ -106,7 +106,7 @@ runQuery();
 
 
 $query = <<<END
-CREATE TABLE student_pin (
+CREATE TABLE student_auth (
 	student_email		VARCHAR(255)	NOT NULL	UNIQUE,
 	student_rollno		VARCHAR(12)		NOT NULL	UNIQUE,
 	student_pin			SMALLINT(5)		NOT NULL	UNIQUE
