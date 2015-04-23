7f642b890b83e82aa8e77a185a375b8e22bbd770
ahmarsultan <ahmar_sultan@live.com>
2015-04-23 19:17:30 +0500

    Startup on Profile Page

---
+++ application/views/user_profile.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * UniLog project.
+ * UniLog is an on-line educational courseware for the University of Engineering and Technology, Lahore.
+ * Copyright 2015 log inc.
+ */
+?>
+
+<p>something</p>
