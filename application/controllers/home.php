a77cca8177a8e3b40670c2c28497d227657c376f
Abdullah Baig <abdullahbaig456@gmail.com>
2015-04-28 19:52:59 +0500

    Developed sign-in.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -13,37 +13,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  *
  */
 class Home extends CI_Controller {
-	
-	public function index() {
-		$this->load->view('user_wall');
-                
-	}
-        
-        public function wall()
-        {
-            $data['page_title'] = 'Wall';
-            $this->load->view('templates/page_head', $data);            
-            $this->load->view('templates/nav');
-	    $this->load->view('templates/left_nav');
-            $this->load->view('user_wall');
-            $this->load->view('templates/page_foot');
-                
-        }
-        
-        public function notice_board(){
-            $this->load->view('templates/page_head');
-            $this->load->view('templates/nav');
-            $this->load->view('templates/left_nav');
-            $this->load->view('templates/notice_board');
-        }
 
-        public function user()
-        {
-            $data['page_title'] = 'Profile';
-            $this->load->view('templates/page_head', $data);            
-            $this->load->view('templates/nav');
-            $this->load->view('templates/left_nav');
-            $this->load->view('user_profile');
-            $this->load->view('templates/page_foot');
-        }
+	public function index() {
+		$this->wall();
+	}
+
+	public function wall() {
+		$data['page_title'] = 'Home';
+		$this->load->view('templates/page_head', $data);
+		$this->load->view('templates/nav');
+		$this->load->view('templates/left_nav');
+		$this->load->view('user_wall');
+		$this->load->view('templates/page_foot');
+	}
+
+	public function notice_board() {
+		$this->load->view('templates/page_head');
+		$this->load->view('templates/nav');
+		$this->load->view('templates/left_nav');
+		$this->load->view('templates/notice_board');
+	}
+
+	public function user() {
+		$data['page_title'] = 'Profile';
+		$this->load->view('templates/page_head', $data);
+		$this->load->view('templates/nav');
+		$this->load->view('templates/left_nav');
+		$this->load->view('user_profile');
+		$this->load->view('templates/page_foot');
+	}
+
 }
