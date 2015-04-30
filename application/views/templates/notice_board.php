99d5864662daf3e1ebb7c12416e8c2b1ec8eb4ed
asadazam93 <asadazam93@gmail.com>
2015-04-30 17:41:37 +0500

    notice board file upload

--- application/views/templates/notice_board.php
+++ application/views/templates/notice_board.php
@@ -4,11 +4,28 @@
  * UniLog is an on-line educational courseware for the University of Engineering and Technology, Lahore.
  * Copyright 2015 log inc.
  */
+
 $notice = image_path('NoticeBoard.png');
-$image = image_path('sticky.png');
+$image = image_path('sticky-blue.png');
 ?>
 
 <script>
+    function change_image(color) {
+        var dynamic_src = "/unilog/images/";
+        switch (color) {
+            case "red":
+                dynamic_src += "sticky-blue.png";
+                break;
+            case "blue":
+                dynamic_src += "sticky-green.png";
+                break;
+            case "green":
+                dynamic_src += "sticky-pink.png";
+                break;
+        }
+
+        $('#drag1').attr('src', dynamic_src);
+    }
     function allowDrop(ev) {
         ev.preventDefault();
     }
@@ -20,23 +37,28 @@ $image = image_path('sticky.png');
     function drop(ev) {
         ev.preventDefault();
         var data = ev.dataTransfer.getData("text");
-        ev.target.appendChild(document.getElementById(data));
+        /* If you use DOM manipulation functions, their default behaviour it not to 
+         copy but to alter and move elements. By appending a ".cloneNode(true)", 
+         you will not move the original element, but create a copy. */
+        var nodeCopy = document.getElementById(data).cloneNode(true);
+        nodeCopy.id = "newId"; /* We cannot use the same ID */
+        ev.target.appendChild(nodeCopy);
     }
 </script>
 
 <div class="col-md-12" >
 
     <div class="jumbotron" style="background-color: white; min-height: 100px; padding: 20px;border-radius:5px" >
-    <form class='form-group'>
-        <b>Notice Board</b>          
-        <hr style="height:2px;background-color: gray">
-            
-        <input type="text" name="post" >
-        <button type="button" class="btn btn-primary">Submit</button>
-        <button class="dropdown btn btn-primary" type="button">
-            <a href="#" data-toggle="dropdown" class="dropdown-toggle" style="color:white">
-                Color <b class="caret"></b></a>
-                <ul class="dropdown-menu">
+        <form class='form-group'>
+            <b>Notice Board</b>          
+            <hr style="height:2px;background-color: gray">
+
+            <input type="text" name="post" >
+            <button type="button" class="btn btn-primary">Submit</button>
+            <button class="dropdown btn btn-primary" type="button">
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle" style="color:white">
+                    Color <b class="caret"></b></a>
+                <ul value="blue" onchange="change_image(this.value)" class="dropdown-menu">
                     <li><a href="#">Green</a></li>
                     <li><a href="#">Pink</a></li>
                     <li><a href="#">Blue</a></li>
@@ -46,13 +68,17 @@ $image = image_path('sticky.png');
             </button>
             <br>
         </form>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload Image" name="submit">
+        </form>
 
     </div>
 
     <img id="drag1" src="<?php echo $image; ?>" draggable="true"
-         ondragstart="drag(event)" style="width:175px;height:200px" >
-    <img id="drag1" src="<?php echo $image; ?>" draggable="true"
-         ondragstart="drag(event)" style="width:175px;height:200px" >
+         ondragstart="drag(event)" style="width:200px;height:225px; padding-left: 25px" >
+
 
     <div class="col-md-12" id="div1" ondrop="drop(event)" ondragover="allowDrop(event)" style="background-color: white;
          background: url(<?php echo $notice; ?>) no-repeat 2px 2px; background-size: 100% 100%; min-height: 700px;padding: 20px;border-radius:5px" >
