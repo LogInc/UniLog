46384add0f992fe915dc5c12264d87035c329941
asadazam93 <asadazam93@gmail.com>
2015-05-01 11:55:09 +0500

    notice board file upload

--- application/views/templates/notice_board.php
+++ application/views/templates/notice_board.php
@@ -49,11 +49,11 @@ $image = image_path('sticky-blue.png');
 <div class="col-md-12" >
 
     <div class="jumbotron" style="background-color: white; min-height: 100px; padding: 20px;border-radius:5px" >
-        <form class='form-group'>
+        <form class='form-group' >
             <b>Notice Board</b>          
             <hr style="height:2px;background-color: gray">
 
-            <input type="text" name="post" >
+            <input type="text" name="post" value="name" >
             <button type="button" class="btn btn-primary">Submit</button>
             <button class="dropdown btn btn-primary" type="button">
                 <a href="#" data-toggle="dropdown" class="dropdown-toggle" style="color:white">
@@ -75,9 +75,10 @@ $image = image_path('sticky-blue.png');
         </form>
 
     </div>
-
+    <div >
     <img id="drag1" src="<?php echo $image; ?>" draggable="true"
          ondragstart="drag(event)" style="width:200px;height:225px; padding-left: 25px" >
+    </div>
 
 
     <div class="col-md-12" id="div1" ondrop="drop(event)" ondragover="allowDrop(event)" style="background-color: white;
