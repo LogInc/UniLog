8de29376917369acfdc6792f7dec817ffd36cf13
Abdullah Baig <abdullahbaig456@gmail.com>
2015-05-01 17:33:42 +0500

    Modifed profile page to show the type of user instead of university name.

--- application/views/user_profile.php
+++ application/views/user_profile.php
@@ -11,6 +11,18 @@ else
 	$profile_image_path = upload_path('profile_pics/' . $user_data->user_photo);
 
 $profile_image = img($profile_image_path, FALSE, 'class="img-rounded" width="200" alt="unilog logo"');
+
+switch ($user_data->user_type) {
+	case 'user_type_admin':
+		$user_type = 'Administrator';
+		break;
+	case 'user_type_student':
+		$user_type = 'Student';
+		break;
+	case 'user_type_instructor':
+		$user_type = 'Instructor';
+		break;
+}
 ?>
 
 
@@ -32,7 +44,7 @@ $profile_image = img($profile_image_path, FALSE, 'class="img-rounded" width="200
         </div>
         <div class="col-md-8" style="font-size: 17px;padding:10px">  
             <h2><strong><?php echo $user_data->user_first_name . ' ' . $user_data->user_last_name ?></strong></h2>
-            <h3><?php echo "Computer Engineering- University of Engineering and Technology" ?></h3>
+            <h3><?php echo $user_type ?></h3>
             <h4><?php echo $user_data->user_email ?></h4>
         </div>
     </div>
@@ -46,7 +58,7 @@ $profile_image = img($profile_image_path, FALSE, 'class="img-rounded" width="200
                 <div class="form-group">
                     <label for="aims-form"></label>
                     <textarea class="form-control" id="aims-form" rows="8" placeholder="write something about you and your goals in life...">
-<?php echo trim($user_data->user_summary); ?></textarea>
+						<?php echo trim($user_data->user_summary); ?></textarea>
                     <div class="pull-right" style="margin-top:10px">
                         <div class="row">
                             <div class="col-md-6">
@@ -60,7 +72,7 @@ $profile_image = img($profile_image_path, FALSE, 'class="img-rounded" width="200
                 </div>
             </form>
         </div>
-		
+
         <div  class="col-md-12" id="summary-display" style="font-size: 17px">
             <div class="caption">                    
                 <p><a class="label label-default" id="#edit-label" rel="tooltip" title="Zoom">Edit</a>
@@ -83,11 +95,11 @@ $profile_image = img($profile_image_path, FALSE, 'class="img-rounded" width="200
 			$("#summary-edit").show();
 			$("#summary-display").hide();
 		}
-		
+
 		$(".save-button").click(function () {
 			var aims = $("#aims-form").val();
 			document.getElementById("aims-paragraph").innerHTML = aims;
-			$.post('<?php echo base_url(); ?>profile/update_summary', { summary: aims });
+			$.post('<?php echo base_url(); ?>profile/update_summary', {summary: aims});
 
 			if (aims.length !== 0) {
 				$("#summary-edit").hide();
